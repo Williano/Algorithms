5bf716e2ff7334a6b7e508037090eb2dca7c38d3
Williano <paawilly17@gmail.com>
2022-01-26 22:59:07 -0600

    Add test cases to selection sort

--- sort/selection_sort/selection_sort_example.py
+++ sort/selection_sort/selection_sort_example.py
@@ -3,9 +3,21 @@ from selection_sort import SelectionSort
 
 def main():
 
-    selection_sort = SelectionSort(data=[5, 3, 6, 2, 10])
+    tests = [
+        [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 2, 1],
+        [],
+        [1, 5, 8, 9],
+        [234, 3, 1, 56, 34, 12, 9, 12, 1300],
+        [78, 12, 15, 8, 61, 53, 23, 27],
+        [5],
+        [5, 3, 6, 2, 10],
+    ]
 
-    print(selection_sort.selection_sort())
+    for element in tests:
+
+        selection_sort = SelectionSort(data=element)
+
+        print(selection_sort.selection_sort())
 
 
 if __name__ == "__main__":
